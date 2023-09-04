e69725791028df6e6efca6c19291b150bc3f8227
mdfaizan973 <faizan.md9735@gmail.com>
2023-09-04 15:41:09 +0530

    added navbar

---
+++ care_fare_cunsulting/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbars from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbars />
+
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ care_fare_cunsulting/src/Components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+function Navbars() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#">Core Fare Consulting</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">About</Nav.Link>
+            <Nav.Link href="#">Services</Nav.Link>
+            <Nav.Link href="#">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navbars;
